d994c1008ef7a9418a78a8742a37e3d3e4e0b0b0
Aryan Singh <83677526+aru24sin@users.noreply.github.com>
2024-11-05 17:39:47 -0500

    Update player3d.gd

--- src/player/player3d.gd
+++ src/player/player3d.gd
@@ -1,6 +1,5 @@
 extends CharacterBody3D
 
-
 signal dashDid
 
 @export var health := 100.0
@@ -12,23 +11,21 @@ signal dashDid
 @export var vel := Vector3.ZERO
 @export var ySpeed := 0.0
 @export var jumpStrength := 17
-var jumpNum := 0
+@export var extraVelMulti := 150
+@export var maxDashAmt := 2
 @export var maxJumpAmt := 2
 @export var damage := 20
 
+var jumpNum := 0
 var canDash := true
 var dashNum := 0
-@export var extraVelMulti := 150
-@export var maxDashAmt := 2
 var extraVel := Vector3.ZERO
 
 @onready var camera = $head/Camera3D
 @onready var raycast = $head/Camera3D/RayCast3D
 
-
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -36,7 +33,6 @@ func _input(event: InputEvent) -> void:
 		rotation.y += -event.relative.x * MOUSE_SENS
 		
 		$head.rotation.x = clamp($head.rotation.x, deg_to_rad(-80), deg_to_rad(80))
-
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("r"):
@@ -48,8 +44,7 @@ func jump() -> void:
 		ySpeed = jumpStrength
 	elif jumpNum == 1:
 		jumpNum += 1
-		ySpeed = 22
-	
+		ySpeed = 22	
 
 func dashFoward() -> void:
 	emit_signal("dashDid")
@@ -58,9 +53,7 @@ func dashFoward() -> void:
 		ySpeed = 0
 	extraVel += ($head/Camera3D/Marker3D.global_transform.origin - $head/Camera3D.global_transform.origin).normalized() * extraVelMulti
 
-
 func _physics_process(delta: float) -> void:
-	
 	vel = get_dir().rotated( Vector3.UP, rotation.y) * speed
 	if !is_on_floor() :
 		ySpeed += gravity
@@ -85,7 +78,6 @@ func _physics_process(delta: float) -> void:
 		dashFoward()
 	extraVel = lerp( extraVel, Vector3.ZERO, 0.1 )
 	vel += extraVel
-
 	
 	set_velocity(vel)
 	set_up_direction(Vector3.UP)
@@ -99,7 +91,6 @@ func attack(damage):
 		var enemy = raycast.get_collider()
 		if enemy.has_method("take_damage"):
 			enemy.take_damage(damage)
-	
 
 func tweenComplete() -> void:
 	$head/Camera3D/camTweenBack.interpolate_property($head/Camera3D,"fov", 94, 90,0.07,Tween.TRANS_QUART,Tween.EASE_IN,0)
