b2e3d2035b38ad2cc796fce88cf57bc73b2a7a16
Aryan Singh <83677526+aru24sin@users.noreply.github.com>
2024-11-06 15:38:56 -0500

    Working on a stamina feature

--- src/player/player3d.gd
+++ src/player/player3d.gd
@@ -15,6 +15,11 @@ signal dashDid
 @export var maxDashAmt := 2
 @export var maxJumpAmt := 2
 @export var damage := 20
+@export var stamina := 100
+@export var maxStamina := 100  # Maximum stamina
+@export var staminaRegenRate := 5  # Stamina regenerated per second
+@export var staminaDashCost := 20  # Stamina cost for dashing
+@export var staminaRunCost := 10  # Stamina cost per second while running
 
 var jumpNum := 0
 var canDash := true
@@ -31,12 +36,15 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		$head.rotation.x += -event.relative.y * MOUSE_SENS
 		rotation.y += -event.relative.x * MOUSE_SENS
-		
 		$head.rotation.x = clamp($head.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("r"):
 		get_tree().reload_current_scene()
+
+	# Regenerate stamina when not running or dashing
+	if !Input.is_action_pressed("run") and !Input.is_action_pressed("f"):
+		stamina = min(stamina + staminaRegenRate * delta, maxStamina)
 
 func jump() -> void:
 	if jumpNum == 0:
@@ -47,16 +55,19 @@ func jump() -> void:
 		ySpeed = 22	
 
 func dashFoward() -> void:
-	emit_signal("dashDid")
-	dashNum += 1
-	if sign(ySpeed) == -1:
-		ySpeed = 0
-	$AnimationPlayer.play("camera_fov")
-	extraVel += ($head/Camera3D/Marker3D.global_transform.origin - $head/Camera3D.global_transform.origin).normalized() * extraVelMulti
+	if stamina >= staminaDashCost and dashNum < maxDashAmt:
+		stamina -= staminaDashCost
+		emit_signal("dashDid")
+		dashNum += 1
+		if sign(ySpeed) == -1:
+			ySpeed = 0
+		$AnimationPlayer.play("camera_fov")
+		extraVel += ($head/Camera3D/Marker3D.global_transform.origin - $head/Camera3D.global_transform.origin).normalized() * extraVelMulti
+		$AnimationPlayer.play("camera_reset")
 
 func _physics_process(delta: float) -> void:
-	vel = get_dir().rotated( Vector3.UP, rotation.y) * speed
-	if !is_on_floor() :
+	vel = get_dir().rotated(Vector3.UP, rotation.y) * speed
+	if !is_on_floor():
 		ySpeed += gravity
 	else:
 		ySpeed = 0.0
@@ -70,18 +81,18 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("attack"):
 		attack(damage)
 	
-	if Input.is_action_pressed("run"):
+	# Running with stamina check
+	if Input.is_action_pressed("run") and stamina > 0:
 		speed = 35
-	elif Input.is_action_just_released("run"):
-		speed = 20
+		stamina = max(stamina - staminaRunCost * delta, 0)  # Reduce stamina while running
+	else:
+		speed = 20  # Reset to normal speed when not running
 
-	if Input.is_action_just_pressed("f") and (dashNum < maxDashAmt):
+	# Dashing with stamina check
+	if Input.is_action_just_pressed("f"):
 		dashFoward()
-	
-	if Input.is_action_just_released("f"):
-		$AnimationPlayer.play("camera_reset")
 		
-	extraVel = lerp( extraVel, Vector3.ZERO, 0.1 )
+	extraVel = lerp(extraVel, Vector3.ZERO, 0.1)
 	vel += extraVel
 	
 	set_velocity(vel)
@@ -98,13 +109,13 @@ func attack(damage):
 			enemy.take_damage(damage)
 
 func tweenComplete() -> void:
-	$head/Camera3D/camTweenBack.interpolate_property($head/Camera3D,"fov", 94, 90,0.07,Tween.TRANS_QUART,Tween.EASE_IN,0)
+	$head/Camera3D/camTweenBack.interpolate_property($head/Camera3D, "fov", 94, 90, 0.07, Tween.TRANS_QUART, Tween.EASE_IN, 0)
 	$head/Camera3D/camTweenBack.start()
 
 func get_dir() -> Vector3:
-	var dir : Vector3
+	var dir: Vector3
 	dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	angForCamToLearpTo = dir.x*-2.5
+	angForCamToLearpTo = dir.x * -2.5
 	dir.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	XangForCamToLearpTo = dir.z*7.5
+	XangForCamToLearpTo = dir.z * 7.5
 	return dir.normalized()
